09f5a9597ac9acd4ba54a4f7603f89b9abf81d58
mustafa-onur-kilinc <mustafaonurkilinc00@gmail.com>
2026-05-30 21:31:24 +0300

    Add show_chapter function to display chapters in dedicated page

--- main.py
+++ main.py
@@ -9,6 +9,24 @@ from flask import Flask, render_template, abort
 app = Flask(__name__)
 
 
+@app.route("/chapters/<int:chapter_num>")
+def show_chapter(chapter_num: int):
+    chapters = [
+        "It was the best of times, it was the worst of times.",
+        "It was the age of wisdom, it was the age of foolishness.",
+        "It was the spring of hope, it was the winter of despair."
+    ]
+
+    if chapter_num > len(chapters):
+        abort(404)
+
+    return render_template(
+        "chapter.html",
+        chapter_title=f"Chapter {chapter_num}",
+        chapter_text=chapters[chapter_num - 1]
+    )
+
+
 @app.route("/")
 def home_page():
     return render_template("index.html")
