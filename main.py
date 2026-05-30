ab087d4faa4e872e0b9ea154d9e18707e8785555
mustafa-onur-kilinc <mustafaonurkilinc00@gmail.com>
2026-05-30 21:28:52 +0300

    Delete unnecessary spaces at the beginning comment block of main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 Resources used to write this code:
 A Minimal Application n.d., Pallets, accessed 30 May 2026,
 <https://flask.palletsprojects.com/en/stable/quickstart/#a-minimal-application>
-
-
 """
 
 from flask import Flask, render_template
