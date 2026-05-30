6c8aae170b7c04f448f24aab0bc4f5c5bf8cb844
mustafa-onur-kilinc <mustafaonurkilinc00@gmail.com>
2026-05-30 17:43:00 +0300

    Added a very simple Flask application

---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+Resources used to write this code:
+A Minimal Application n.d., Pallets, accessed 30 May 2026,
+<https://flask.palletsprojects.com/en/stable/quickstart/#a-minimal-application>
+
+
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home_page():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
